0e6b6f4aba1a8de29ad3212246497de3431dbe08
JChover <jesuschover@gmail.com>
2025-02-17 16:46:37 +0100

    Progress in Chapter 9.
    CP: 169 Defining Attributes and Methods for the Child Class.

---
+++ PART I - BASICS/Chapter 9 - Classes/Files/electric_car.py
@@ -0,0 +1,33 @@
+# The __init__() Method for a Child Class - electric_car.py
+class Car:
+    # This is the Car Class from car.py that we will use as Parent to create our Child Class
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
